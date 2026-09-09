23b88ded5ef733b00b4a1fbb286e07b0ad4e8d93
Zen <riad16x@gmail.com>
2026-09-09 10:20:02 +0100

    c

--- Database/saoif_active_events_auto.js
+++ Database/saoif_active_events_auto.js
@@ -37,12 +37,30 @@ const ACTIVE_EVENTS_AUTO = [
   },
   {
     "type": "Campaign",
+    "title": "[Worldwide 8.5 Year Anniversary] \"Multiple Rounds: 5x Experience Subquest Campaign\" On Now!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260909sq2.png",
+    "startTime": "2026-09-08T19:00:00+00:00",
+    "endTime": "2026-09-15T19:00:00+00:00",
+    "snippet": "Sword Art Online Integral Factor's Worldwide 8.5 Year Anniversary! During this campaign, get 5x the experience in Multiple Rounds Subquests! This is your chance to easily raise your player level!  Campaign Duration After September 9, 2026 (Wed) maintenance - Before September 16, 2026 (Wed) maintenan...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5443.html"
+  },
+  {
+    "type": "Campaign",
     "title": "[Worldwide 8.5 Year Anniversary] \"Guild Support Campaign\" is coming! You can use limited-time Guild Skills!",
     "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260902gc.png",
     "startTime": "2026-09-01T19:00:00+00:00",
     "endTime": "2026-09-15T19:00:00+00:00",
     "snippet": "Sword Art Online Integral Factor's Worldwide 8.5 Year Anniversary! Use limited-time Guild Skills in the Guild Support Campaign. It's a great opportunity to join a guild or recruit new members.  Limited-Time Guild Skills For a limited-time, use Guild Skills that add Production's Additional Bonus Up o...",
     "news_url": "https://saoif.nayu.fun/shop_information/detail/5433.html"
+  },
+  {
+    "type": "EVENT",
+    "title": "[9/9 14:00 (JST/UTC+9) UPDATE] Added high-difficulty \"Abyss\" and rewards!The \"Lightning Horse Hunt\" is on! Get fantastic items! You can get tons of Memoria Niter to Limit Break Skill Records, as well as transformation and enhancement materials!<",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260902lh.png",
+    "startTime": "2026-09-01T19:00:00+00:00",
+    "endTime": "2026-09-28T19:00:00+00:00",
+    "snippet": "[9/9 14:00 (JST/UTC+9) UPDATE] The high-difficulty dungeon \"Abyss\" and items were added during the September 9, 2026 (Wed) maintenance.  High-difficulty dungeon, \"Abyss\" added This is a Crystal-Disabled Area where Revive Crystals cannot be used. The recommended Total Power is 600,000. You can comple...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5444.html"
   },
   {
     "type": "EVENT",
@@ -61,5 +79,14 @@ const ACTIVE_EVENTS_AUTO = [
     "endTime": "2026-09-28T19:00:00+00:00",
     "snippet": "To celebrate the upcoming launch of the newest game of the Sword Art Online Game Series, we will be holding a special event in SAOIF!  Echoes of Survival is an event that recreates the unforgiving Death Game Mode to be implemented in Echoes of Aincrad. Defeat will not be an option. Entering a Near-D...",
     "news_url": "https://saoif.nayu.fun/shop_information/detail/5288.html"
+  },
+  {
+    "type": "EVENT",
+    "title": "\"Search for the Phantom Mushroom\" is back! Get each Bear Avatar and a Weapon Avatar with glowing special effects!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260909ki.png",
+    "startTime": "2026-09-08T19:00:00+00:00",
+    "endTime": "2026-10-06T19:00:00+00:00",
+    "snippet": "The Scenario Event \"Search for the Phantom Mushroom\" is back!  Search for the Phantom Mushroom Looking to raise his level, Kirito ventured into a forest area. There, he discovered a strange mushroom sprouting from a cliff. According to Argo, these mushrooms only grow on cliffs and precipices, have a...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5445.html"
   }
 ];
