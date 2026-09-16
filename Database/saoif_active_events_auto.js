f325a04fc78a1d429f90c655e79c8734557694b5
Zen <riad16x@gmail.com>
2026-09-16 10:22:18 +0100

    main 16/09

--- Database/saoif_active_events_auto.js
+++ Database/saoif_active_events_auto.js
@@ -28,6 +28,33 @@ const ACTIVE_EVENTS_AUTO = [
   },
   {
     "type": "EVENT",
+    "title": "[9/16 14:00 (JST/UTC+9) UPDATE] New items added to the Trader! The \"Lightning Horse Hunt\" is on! Get fantastic items! You can get tons of Memoria Niter to Limit Break Skill Records, as well as transformation and enhancement materials!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260902lh.png",
+    "startTime": "2026-09-01T19:00:00+00:00",
+    "endTime": "2026-09-28T19:00:00+00:00",
+    "snippet": "[9/16 14:00 (JST/UTC+9) UPDATE] Items have been added to the Trader. Exchange \"Red Lightning Plate\" for the added items.  Added Items Tradable Item ListAmount NeededTrade Limit Memoria Niter S10040 Record Frame L650 Glare Dragon Stone625 Recovery Potion325 Life Potion95 First Aid Crystal310 Purifica...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5460.html"
+  },
+  {
+    "type": "Campaign",
+    "title": "[Worldwide 8.5 Year Anniversary] The \"Boss Ingot Increased Drop Rate Campaign\" is here! Increased drop rate for Metal Scraps needed to produce Boss Ingots!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260916ingot.png",
+    "startTime": "2026-09-15T19:00:00+00:00",
+    "endTime": "2026-09-28T19:00:00+00:00",
+    "snippet": "Sword Art Online Integral Factor's Worldwide 8.5 Year Anniversary! The Boss Ingot Increased Drop Rate Campaign is on now!  The drop rate for Metal Scraps needed to produce Boss Ingots is increased for a limited time! Work with your party and collect Metal Scraps!  Main Boss Ingots       In the dunge...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5459.html"
+  },
+  {
+    "type": "EVENT",
+    "title": "[Worldwide 8.5 Year Anniversary] \"Silica's Heapin' 50M EXP Event\" on now!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260916gp2.png",
+    "startTime": "2026-09-15T19:00:00+00:00",
+    "endTime": "2026-09-28T19:00:00+00:00",
+    "snippet": "Sword Art Online Integral Factor's Worldwide 8.5 Year Anniversary! You can earn tons of EXP in the special quest on now!  Accept the quest, move to the designated map, and clear it by defeating the monsters to claim your rewards. Plus, the difficulty increases as you progress through the quest.  Rew...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5458.html"
+  },
+  {
+    "type": "EVENT",
     "title": "[9/9 14:00 (JST/UTC+9) UPDATE] Added high-difficulty \"Abyss\" and rewards!The \"Lightning Horse Hunt\" is on! Get fantastic items! You can get tons of Memoria Niter to Limit Break Skill Records, as well as transformation and enhancement materials!<",
     "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260902lh.png",
     "startTime": "2026-09-01T19:00:00+00:00",
@@ -61,5 +88,14 @@ const ACTIVE_EVENTS_AUTO = [
     "endTime": "2026-10-06T19:00:00+00:00",
     "snippet": "The Scenario Event \"Search for the Phantom Mushroom\" is back!  Search for the Phantom Mushroom Looking to raise his level, Kirito ventured into a forest area. There, he discovered a strange mushroom sprouting from a cliff. According to Argo, these mushrooms only grow on cliffs and precipices, have a...",
     "news_url": "https://saoif.nayu.fun/shop_information/detail/5445.html"
+  },
+  {
+    "type": "EVENT",
+    "title": "[Worldwide 8.5 Year Anniversary] \"Dinosaur Assault\" is here! Earn Arcana Gems, Awakening Stars, Medals of Combat Arts, Exp Crystals, and more!",
+    "image": "https://saoif-com.akamaized.net/web/shop_login/en/img/other/20260916da.png",
+    "startTime": "2026-09-15T19:00:00+00:00",
+    "endTime": "2026-10-14T19:00:00+00:00",
+    "snippet": "Sword Art Online Integral Factor's Worldwide 8.5 Year Anniversary!  Hordes of monsters attack in the \"Dinosaur Assault\"! After a certain amount of time passes, the boss monster will appear. Drive off hordes of monsters and earn tons of EXP!  Rewards In addition to tons of EXP and the \"Exp Crystals\" ...",
+    "news_url": "https://saoif.nayu.fun/shop_information/detail/5457.html"
   }
 ];
